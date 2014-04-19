4b2aa18eac7a3deba719b47b049d6fb0edd7fcb4
Tylor Garrett <tylorgarrett@gmail.com>
2014-04-19 17:50:43 -0400

    Added ImageHelper and fixed/added things for circular images in GameLobby

--- android/voltag/src/main/java/edu/purdue/voltag/fragments/GameLobbyFragment.java
+++ android/voltag/src/main/java/edu/purdue/voltag/fragments/GameLobbyFragment.java
@@ -1,14 +1,19 @@
 package edu.purdue.voltag.fragments;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import edu.purdue.voltag.R;
+import edu.purdue.voltag.helper.ImageHelper;
 
 /*
  * A simple {@link android.support.v4.app.Fragment} subclass.
@@ -26,9 +31,11 @@ public class GameLobbyFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.david);
+        ImageView imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+        imageView.setImageBitmap(ImageHelper.getRoundedCornerBitmap(bitmap, 500));
         return inflater.inflate(R.layout.fragment_game_lobby, container, false);
     }
 
