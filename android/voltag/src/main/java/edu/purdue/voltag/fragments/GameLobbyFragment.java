286b5f7a2fecfbac99df68055164f72b06f133e9
David Tschida <dmtschida1@gmail.com>
2014-04-19 20:22:47 -0400

    Pulled from gravitar

--- android/voltag/src/main/java/edu/purdue/voltag/fragments/GameLobbyFragment.java
+++ android/voltag/src/main/java/edu/purdue/voltag/fragments/GameLobbyFragment.java
@@ -3,20 +3,22 @@ package edu.purdue.voltag.fragments;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.graphics.drawable.Drawable;
 import android.app.ListFragment;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ArrayAdapter;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import edu.purdue.voltag.R;
+import edu.purdue.voltag.data.Player;
 import edu.purdue.voltag.helper.ImageHelper;
 
 /*
@@ -38,16 +40,48 @@ public class GameLobbyFragment extends ListFragment {
     public void onAttach(Activity activity)
     {
         super.onAttach(activity);
-        setListAdapter(new ArrayAdapter<String>(activity, R.layout.player_list_item, R.id.name, new String[] {"David", "Tylor", "Kyle", "Cartman", "Michael"} ));
+        setListAdapter(new ArrayAdapter<String>(activity, R.layout.player_list_item, R.id.name, new String[]{"David", "Tylor", "Kyle", "Cartman", "Michael"}));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.david);
-        ImageView imageView = (ImageView) getActivity().findViewById(R.id.imageView);
-        imageView.setImageBitmap(ImageHelper.getRoundedCornerBitmap(bitmap, 500));
-        return inflater.inflate(R.layout.fragment_game_lobby, container, false);
+        final View v = inflater.inflate(R.layout.fragment_game_lobby, container, false);
+
+        final Player p = new Player(null, null, null, "dmtschida1@gmail.com");
+
+        new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... params) {
+                final String baseUrl = "http://www.gravatar.com/avatar/";
+                final String processedAddress = p.getEmail();
+
+                final String hashCode = p.getGravitarURL();
+                final String url = baseUrl + hashCode+"?s=" + "180" + "&d=blank";
+
+                HttpURLConnection httpURLConnection = null;
+                try {
+                    httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
+                    final InputStream is = httpURLConnection.getInputStream();
+                    Bitmap gravitar = BitmapFactory.decodeStream(is, null, new BitmapFactory.Options());
+                    return ImageHelper.getRoundedCornerBitmap(gravitar, 500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    httpURLConnection.disconnect();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                ((ImageView)v.findViewById(R.id.imageView)).setImageBitmap(bitmap);
+            }
+        }.execute();
+
+
+        return v;
     }
+
 
 }
