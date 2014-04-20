a5b7c38ffaa7dd5d8606ec861c098bc4fb5a8e93
Michael Hockerman <mike@hockerman.com>
2014-04-20 16:46:06 -0400

    asdfasdfasdfasdf

--- android/voltag/src/main/java/edu/purdue/voltag/data/VoltagDB.java
+++ android/voltag/src/main/java/edu/purdue/voltag/data/VoltagDB.java
@@ -434,7 +434,7 @@ public class VoltagDB extends SQLiteOpenHelper{
 
                 // Re-fill it with new players
                 for (ParseObject p : players) {
-                    String playerID = p.getString(ParseConstants.CLASS_ID);
+                    String playerID = p.getString(p.getObjectId());
                     String hardwareID = p.getString(ParseConstants.PLAYER_HARDWARE_ID);
                     String playerName = p.getString(ParseConstants.PLAYER_NAME);
                     String playerEmail = p.getString(ParseConstants.PLAYER_EMAIL);
