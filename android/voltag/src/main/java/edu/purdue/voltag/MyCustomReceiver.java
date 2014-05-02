022301086a9c7d185e26308a43f62924238ed64a
David Tschida <dmtschida1@gmail.com>
2014-05-02 00:21:11 -0400

    Added notification

--- android/voltag/src/main/java/edu/purdue/voltag/MyCustomReceiver.java
+++ android/voltag/src/main/java/edu/purdue/voltag/MyCustomReceiver.java
@@ -1,9 +1,11 @@
 package edu.purdue.voltag;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -12,30 +14,56 @@ import org.json.JSONObject;
 import java.util.Iterator;
 
 public class MyCustomReceiver extends BroadcastReceiver {
-private static final String TAG = "MyCustomReceiver";
- 
-  @Override
-  public void onReceive(Context context, Intent intent) {
-    try {
-      String action = intent.getAction();
-      String channel = intent.getExtras().getString("com.parse.Channel");
-        if(channel == null){
-            return;
+    private static final String TAG = "MyCustomReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            String action = intent.getAction();
+            String channel = intent.getExtras().getString("com.parse.Channel");
+            if (channel == null) {
+                return;
+            }
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+
+            Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+            Iterator itr = json.keys();
+            while (itr.hasNext()) {
+                String key = (String) itr.next();
+                Log.d(TAG, "..." + key + " => " + json.getString(key));
+            }
+
+            Intent i = new Intent();
+            i.setClassName("edu.purdue.voltag", "edu.purdue.voltag.MainActivity");
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .setContentText(intent.getStringExtra("alert"));
+
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getActivity(
+                            context,
+                            0,
+                            i,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+
+            mBuilder.setContentIntent(resultPendingIntent);
+
+            // Sets an ID for the notification
+            int mNotificationId = 001;
+            // Gets an instance of the NotificationManager service
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // Builds the notification and issues it.
+            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
+
+            context.startActivity(i);
+        } catch (JSONException e) {
+            Log.d(TAG, "JSONException: " + e.getMessage());
         }
-      JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
- 
-      Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
-      Iterator itr = json.keys();
-      while (itr.hasNext()) {
-        String key = (String) itr.next();
-        Log.d(TAG, "..." + key + " => " + json.getString(key));
-      }
-        Intent i = new Intent();
-        i.setClassName("edu.purdue.voltag", "edu.purdue.voltag.MainActivity");
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);
-    } catch (JSONException e) {
-      Log.d(TAG, "JSONException: " + e.getMessage());
     }
-  }
 }
